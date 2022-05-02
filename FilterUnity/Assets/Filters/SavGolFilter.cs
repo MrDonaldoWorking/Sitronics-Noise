3a9c188a2d1fdb820a5c831b04c059f86557f210
MrDonaldoPublic <mr.donaldo.cf@gmail.com>
2022-05-02 09:18:11 +0900

    Implemented full Savitzky Golay

--- FilterUnity/Assets/Filters/SavGolFilter.cs
+++ FilterUnity/Assets/Filters/SavGolFilter.cs
@@ -6,7 +6,8 @@ using System.IO;
 // How it generated described here: https://github.com/mathnet/mathnet-filtering/issues/4
 public class SavGolFilter
 {
-    private readonly ArrayList coefficients;
+    private readonly float[,] coefsMatrix;
+    private readonly float[,] s;
     private readonly int minPoints = 2;
     private readonly int maxPoints = 10;
     private readonly int sidePoints;
@@ -16,13 +17,16 @@ public class SavGolFilter
     {
         this.sidePoints = sidePoints;
         this.degree = degree;
+        ArrayList coefficients = new ArrayList();
         using (TextReader reader = File.OpenText("./SavGolC.conf"))
         {
             for (int sPoints = minPoints; sPoints <= maxPoints; ++sPoints)
             {
-                string line = reader.ReadLine();
+                string line = reader.ReadLine() ?? throw new ArgumentException("Expected new line");
                 string[] datas = line.Split(':');
                 int check = int.Parse(datas[1]);
+
+                //Console.WriteLine($"From file: {datas[0]}");
 
                 string[] coefs = datas[0].Split(' ');
                 int[] raws = new int[coefs.Length];
@@ -42,27 +46,91 @@ public class SavGolFilter
                 float[] curCoefs = new float[coefs.Length * 2 - 1];
                 for (int i = 0; i < coefs.Length; ++i)
                 {
-                    curCoefs[i] = (float)raws[i] / coefs.Length;
+                    curCoefs[i] = (float)raws[i] / check;
                 }
                 for (int i = 0; i < coefs.Length - 1; ++i)
                 {
-                    curCoefs[i] = curCoefs[coefs.Length - 2 - i];
+                    curCoefs[i + coefs.Length] = curCoefs[coefs.Length - 2 - i];
                 }
+                //Console.WriteLine($"converted to: {String.Join(" ", curCoefs.Cast<float>())}");
                 coefficients.Add(curCoefs);
+            }
+        }
+
+        // matrix with degrees from 0 to sidePoints of [-m..m]
+        float[,] s = new float[sidePoints * 2 + 1, degree + 1];
+        for (int i = 0; i <= degree; ++i)
+        {
+            for (int m = -sidePoints; m <= sidePoints; ++m)
+            {
+                s[m + sidePoints, i] = (float)Math.Pow(m, i);
+            }
+        }
+        this.s = s;
+
+        // Square matrix
+        float[,] sq = LinearAlgebra.TransposeAndMultiplyWith(ref s, ref s);
+        //Console.WriteLine($"sq: ");
+        //Print2DArray(sq);
+        // inverted matrix
+        float[,] inv = LinearAlgebra.Inverse(sq);
+        //Console.WriteLine($"inv: ");
+        //Print2DArray(inv);
+        // multiplied with inversed
+        float[,] mul = LinearAlgebra.Multiply(ref s, ref inv);
+        //Console.WriteLine("mul: ");
+        //Print2DArray(mul);
+        // Сalculated coefficients
+        coefsMatrix = LinearAlgebra.MultiplyWithTransposed(ref mul, ref s);
+        //Console.WriteLine($"coefs: ");
+        //Print2DArray(coefsMatrix);
+
+        if (degree == 2 || degree == 3)
+        {
+            float[] flt = coefficients[sidePoints - minPoints] as float[];
+            for (int i = 0; i < coefsMatrix.GetLength(1); ++i)
+            {
+                if (Math.Abs(coefsMatrix[sidePoints, i] - flt[i]) >= 1e-5)
+                {
+                    Console.WriteLine("Coefficients differ!");
+                    Console.WriteLine($"from file: {String.Join(" ", flt.Cast<float>())}");
+                    throw new ArgumentException("From file differs");
+                }
             }
         }
     }
 
-    public float[] Filter(ArrayList vals, int len)
+    public static void Print2DArray(float[,] matrix)
     {
-        float[] coefs = coefficients[sidePoints - minPoints] as float[];
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j].ToString("f7") + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public float[,] GetS()
+    {
+        return s;
+    }
+
+    public float[,] GetCoefsMatrix()
+    {
+        return coefsMatrix;
+    }
+
+    public float[] Filter(ref ArrayList vals, int len)
+    {
         float[] res = new float[len];
         for (int q = 0; q < len; ++q)
         {
             for (int i = 0; i < vals.Count; ++i)
             {
-                float[] curVals = vals[i] as float[];
-                res[q] += coefs[i] * curVals[q];
+                float[] curVals = vals[i] as float[] ?? throw new ArgumentException($"vals[{i}] is null");
+                res[q] += coefsMatrix[i, sidePoints] * curVals[q];
             }
         }
         return res;
